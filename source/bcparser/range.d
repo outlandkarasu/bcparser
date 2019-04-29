bdce297b76a0b837530aabde7433e5249278c3a4
outlandkarasu <outland.karasu@gmail.com>
2019-04-29 11:41:06 +0900

    issue #6 add isBetterCInputRange enum.

--- source/bcparser/range.d
+++ source/bcparser/range.d
@@ -1,6 +1,57 @@
 module bcparser.range;
 
-import std.range : isForwardRange;
+import std.range :
+    isForwardRange,
+    isInputRange;
+
+/**
+Returns `true` if `R` is a BetterC forward range.
+*/
+enum bool isBetterCInputRange(R) =
+    isInputRange!R
+    && is(typeof((R r) @nogc nothrow => r.empty))
+    && is(typeof((R r) @nogc nothrow => r.front))
+    && is(typeof((R r) @nogc nothrow => r.popFront));
+
+///
+@nogc nothrow @safe unittest
+{
+    @safe struct Range
+    {
+        nothrow @property pure char front() const { return char.init; }
+        nothrow pure void popFront() { }
+        nothrow @property pure bool empty() const { return false; }
+    }
+    static assert(isBetterCInputRange!Range);
+
+    version (D_BetterC) { }
+    else
+    {
+        @safe struct FrontNoBCRange
+        {
+            nothrow @property pure char front() const { new Object; return char.init; }
+            @nogc nothrow pure void popFront() { }
+            @nogc nothrow @property pure bool empty() const { return false; }
+        }
+        static assert(!isBetterCInputRange!FrontNoBCRange);
+
+        @safe struct PopFrontNoBCRange
+        {
+            @nogc nothrow @property pure char front() const { return char.init; }
+            nothrow pure void popFront() { new Object; }
+            @nogc nothrow @property pure bool empty() const { return false; }
+        }
+        static assert(!isBetterCInputRange!PopFrontNoBCRange);
+
+        @safe struct EmptyNoBCRange
+        {
+            @nogc nothrow @property pure char front() const { return char.init; }
+            @nogc nothrow pure void popFront() { }
+            nothrow @property pure bool empty() const { new Object; return false; }
+        }
+        static assert(!isBetterCInputRange!EmptyNoBCRange);
+    }
+}
 
 /**
 Returns `true` if `R` is a BetterC forward range.
