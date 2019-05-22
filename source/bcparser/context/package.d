69473aade55beb19a3c2d8a9b3689069ace3f04c
outlandkarasu <outland.karasu@gmail.com>
2019-05-22 23:52:03 +0900

    issue #13 add source position.

---
+++ source/bcparser/context/package.d
@@ -0,0 +1,7 @@
+/**
+Parsing context package.
+*/
+module bcparser.context;
+
+public import bcparser.context.source_position;
+
---
+++ source/bcparser/context/source_position.d
@@ -0,0 +1,17 @@
+/**
+Source position structure.
+*/
+module bcparser.context.source_position;
+
+/**
+Source position.
+*/
+struct SourcePosition
+{
+    /// element index.
+    size_t index;
+
+    /// line number.
+    size_t line;
+}
+
