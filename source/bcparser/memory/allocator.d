0b958ca63b2c61115a54819cfdec8a2f6fb8c3eb
outlandkarasu <outland.karasu@gmail.com>
2019-06-27 23:55:33 +0900

    issue #17 add allocator methods.

--- source/bcparser/memory/allocator.d
+++ source/bcparser/memory/allocator.d
@@ -42,3 +42,96 @@ enum bool isAllocator(A) =
     static assert(!isAllocator!WrongGC);
 }
 
+/**
+release elements array.
+
+Params:
+    A = allocator type.
+    E = element type.
+    allocator = memory allocator.
+    array = dest array.
+*/
+void release(A, E)(
+    scope return ref A allocator,
+    scope return ref E[] array) @nogc nothrow @safe
+{
+    void[] memory = array;
+    allocator.free(memory);
+    array = null;
+}
+
+///
+@nogc nothrow @trusted unittest
+{
+    import bcparser.memory : CAllocator;
+
+    auto allocator = CAllocator();
+
+    // allocate memory.
+    void[] memory;
+    allocator.allocate(memory, 16);
+    auto array = cast(char[]) memory;
+    assert(array.length == 16);
+
+    // release memory.
+    allocator.release(array);
+    assert(array.length == 0);
+}
+
+/**
+add an element.
+
+Params:
+    A = allocator type.
+    E = element type.
+    allocator = memory allocator.
+    array = dest array.
+    element = add element.
+Returns:
+    true if succeeded.
+*/
+bool add(A, E)(
+    scope return ref A allocator,
+    scope return ref E[] array,
+    auto scope return ref E element) @nogc nothrow @trusted
+{
+    void[] memory = array;
+    bool success;
+    if (memory.length > 0)
+    {
+        success = allocator.resize(memory, memory.length + E.sizeof);
+    }
+    else
+    {
+        success = allocator.allocate(memory, E.sizeof);
+    }
+
+    if (!success)
+    {
+        return false;
+    }
+
+    array = cast(E[]) memory;
+    array[$ - 1] = element;
+    return true;
+}
+
+///
+@nogc nothrow @safe unittest
+{
+    import bcparser.memory : CAllocator;
+
+    char[] array;
+    auto allocator = CAllocator();
+    allocator.add(array, 'a');
+    scope(exit) allocator.release(array);
+
+    assert(array.length == 1);
+    assert(array[0] == 'a');
+
+    allocator.add(array, 'b');
+    allocator.add(array, 'c');
+    assert(array.length == 3);
+    assert(array == "abc");
+}
+
