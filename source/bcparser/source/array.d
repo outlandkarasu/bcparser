c2aa31732be8e5686d7cff263ea0cd6358d93595
outlandkarasu <outland.karasu@gmail.com>
2019-06-27 22:49:22 +0900

    issue #17 add attributes.

--- source/bcparser/source/array.d
+++ source/bcparser/source/array.d
@@ -19,7 +19,7 @@ struct ArraySource(E)
         this.array_ = array;
     }
 
-    bool next(out E e) @nogc nothrow pure @safe
+    bool next(return scope out E e) @nogc nothrow pure @safe
     {
         if (position_ >= array_.length)
         {
