c44f8736d1bccc8ebcfff48df6046f75c3ede367
outlandkarasu <outland.karasu@gmail.com>
2019-06-30 22:41:32 +0900

    issue #20 add package.

--- source/bcparser/parsers/package.d
+++ source/bcparser/parsers/package.d
@@ -9,4 +9,4 @@ public import bcparser.parsers.parse_empty;
 public import bcparser.parsers.parse_range;
 public import bcparser.parsers.parse_set;
 public import bcparser.parsers.parse_string;
-
+public import bcparser.parsers.traits;
